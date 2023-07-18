c98725832024f67a7c35ddeace147c08d6a9b125
MrWeasel <82470090+MrWeasel1000@users.noreply.github.com>
2023-07-18 15:00:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,72 @@
-// JavaScript code for the Netflix-like Trailer Showcase
-const trailerSection = document.getElementById('trailer-section');
+// Refresh function to shuffle videos
+function refreshVideos() {
+  const container = document.getElementById('videoContainer');
+  const videos = Array.from(container.querySelectorAll('.video-section'));
 
-// Create an array with the video URLs
-const videoUrls = [
-  'https://youtu.be/ex3C1-5Dhb8',
-  // Add more video URLs here if needed
-];
+  // Randomize the order of videos
+  for (let i = videos.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    container.appendChild(videos[j]);
+  }
 
-// Dynamically generate YouTube embeds for each video URL
-videoUrls.forEach((videoUrl) => {
-  const iframe = document.createElement('iframe');
-  iframe.src = `https://www.youtube.com/embed/${videoUrl.slice(-11)}`;
-  iframe.width = '560';
-  iframe.height = '315';
-  iframe.frameBorder = '0';
-  iframe.allow = 'autoplay; encrypted-media';
-  iframe.allowFullscreen = true;
+  // Show loading animation
+  const loading = document.getElementById('loading');
+  loading.style.display = 'flex';
 
-  trailerSection.appendChild(iframe);
+  // Hide loading animation after a short delay (simulating loading time)
+  setTimeout(function() {
+    loading.style.display = 'none';
+    const allVideos = document.querySelectorAll('.youtube-video');
+    allVideos.forEach(function(video, index) {
+      if (index === 0) {
+        video.setAttribute('src', video.getAttribute('src') + '?autoplay=1');
+      } else {
+        video.setAttribute('src', video.getAttribute('src').replace('?autoplay=1', ''));
+      }
+    });
+    container.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, 1500);
+}
+
+// Scroll snap navigation using arrow keys
+window.addEventListener('keydown', function(e) {
+  const container = document.querySelector('.video-container');
+  if (e.key === 'ArrowUp') {
+    container.scrollBy({
+      top: -window.innerHeight,
+      behavior: 'smooth'
+    });
+  } else if (e.key === 'ArrowDown') {
+    container.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth'
+    });
+  }
+});
+
+// Scroll snap navigation using touch gestures
+let touchStartY = 0;
+window.addEventListener('touchstart', function(e) {
+  touchStartY = e.touches[0].clientY;
+});
+
+window.addEventListener('touchend', function(e) {
+  const container = document.querySelector('.video-container');
+  const touchEndY = e.changedTouches[0].clientY;
+  const deltaY = touchEndY - touchStartY;
+
+  if (deltaY < -50) {
+    container.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth'
+    });
+  } else if (deltaY > 50) {
+    container.scrollBy({
+      top: -window.innerHeight,
+      behavior: 'smooth'
+    });
+  }
 });
