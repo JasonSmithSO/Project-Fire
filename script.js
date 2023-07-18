de1ed77ab14f0d46d69451507f316b2fe987fda1
MrWeasel <82470090+MrWeasel1000@users.noreply.github.com>
2023-07-18 14:48:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,72 @@
-// JavaScript code for the TikTok-like Trailer Showcase
-const trailerSection = document.getElementById('trailer-section');
+// Refresh function to shuffle videos
+function refreshVideos() {
+  const container = document.getElementById('videoContainer');
+  const videos = Array.from(container.querySelectorAll('.video-section'));
 
-// Fetch trailer data from an API or use static data
-// Here's a simple example using static data
-const trailers = [
-  { videoUrl: 'https://www.youtube.com/embed/ex3C1-5Dhb8' },
-  // Add more trailer data here
-];
+  // Randomize the order of videos
+  for (let i = videos.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    container.appendChild(videos[j]);
+  }
 
-// Dynamically generate TikTok-like video embeds for each trailer
-trailers.forEach((trailer) => {
-  const videoContainer = document.createElement('div');
-  videoContainer.className = 'video-container';
+  // Show loading animation
+  const loading = document.getElementById('loading');
+  loading.style.display = 'flex';
 
-  const iframe = document.createElement('iframe');
-  iframe.src = trailer.videoUrl;
-  iframe.frameBorder = '0';
-  iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
-  iframe.allowFullscreen = true;
+  // Hide loading animation after a short delay (simulating loading time)
+  setTimeout(function() {
+    loading.style.display = 'none';
+    const allVideos = document.querySelectorAll('.youtube-video');
+    allVideos.forEach(function(video, index) {
+      if (index === 0) {
+        video.setAttribute('src', video.getAttribute('src') + '?autoplay=1');
+      } else {
+        video.setAttribute('src', video.getAttribute('src').replace('?autoplay=1', ''));
+      }
+    });
+    container.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, 1500);
+}
 
-  videoContainer.appendChild(iframe);
-  trailerSection.appendChild(videoContainer);
+// Scroll snap navigation using arrow keys
+window.addEventListener('keydown', function(e) {
+  const container = document.querySelector('.video-container');
+  if (e.key === 'ArrowUp') {
+    container.scrollBy({
+      top: -window.innerHeight,
+      behavior: 'smooth'
+    });
+  } else if (e.key === 'ArrowDown') {
+    container.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth'
+    });
+  }
+});
+
+// Scroll snap navigation using touch gestures
+let touchStartY = 0;
+window.addEventListener('touchstart', function(e) {
+  touchStartY = e.touches[0].clientY;
+});
+
+window.addEventListener('touchend', function(e) {
+  const container = document.querySelector('.video-container');
+  const touchEndY = e.changedTouches[0].clientY;
+  const deltaY = touchEndY - touchStartY;
+
+  if (deltaY < -50) {
+    container.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth'
+    });
+  } else if (deltaY > 50) {
+    container.scrollBy({
+      top: -window.innerHeight,
+      behavior: 'smooth'
+    });
+  }
 });
